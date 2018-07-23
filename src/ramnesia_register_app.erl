1157094658c80872d4c55728ab92adc42021250f
Daniil Fedotov <hairyhum@gmail.com>
2018-07-23 17:36:10 +0100

    wip

--- src/ramnesia_register_app.erl
+++ src/ramnesia_register_app.erl
@@ -5,6 +5,7 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    error_logger:error_msg("Members ~p~n", [ra:members(ramnesia_node:node_id())]),
     ramnesia_node:trigger_election(),
     ramnesia_jepsen_api:start(),
     case string:split(atom_to_list(node()), "@") of
